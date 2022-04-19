b15501e71d997661b8bd6f9bbc382036272b2d0c
Носов Артём Иванович Б20-514 <nai008campus.mephi.ru>
2022-04-19 21:00:03 +0300

    For newbees

--- Lab2_Nosov_1k_2s_04.04.2021/Main.cpp
+++ Lab2_Nosov_1k_2s_04.04.2021/Main.cpp
@@ -27,38 +27,69 @@ using namespace std;
 // − Перегрузка операторов (например, оператора [] для обращения/задания значения элемента по индексу).
 
 //Меню для лабароторной работы
-string strForListMenuLevel0[3] = {"целыми числами", "комплексными числами", "строками"};
-UIMenu menuLab2_lvl_0((int)3, strForListMenuLevel0);
+
+
 
 int main()
 {
+    string strForListMenuLevel0[3] = { "целыми числами", "комплексными числами", "строками" };
+    UIMenu menuLab2_lvl_0((int)3, strForListMenuLevel0);
     setlocale(LC_ALL, "Russian");
     int userСhoice = menuLab2_lvl_0.userСhoice("***Главное меню***\n\nТестирование коллекций с:");
     if (userСhoice != 0)
         testQueueUI(userСhoice);
-    //LinkedListSequence<float>* mySeq = new LinkedListSequence<float>;
-    //float arr[3] = { 1.5, 2.3, 3.14 };
-    /*mySeq->Append(arr[0]);
-    mySeq->Append(arr[1]);
-    mySeq->Append(arr[2]);
-    cout << "Элемент под номером нуль " << mySeq->Get(0) << endl;
-    cout << "Элемент под номером один " << mySeq->Get(1) << endl;
-    cout << "Элемент под номером два " << mySeq->Get(2)<<endl;
-    cout << "Длинна списка " << mySeq->GetLength() << endl;*/
-
-    //Queue<float>* que = new Queue<float>;
-    //que->push(arr[0]);
-    //que->push(arr[1]);
-    //que->push(arr[2]);
-    //que->printQueue();
-    //cout << "Элемент под номером нуль " << que->pop() << endl;
-    //cout << "Элемент под номером один " << que->pop() << endl;
-    //cout << "Элемент под номером два " << que->pop() << endl;
-    //cout << "Длинна списка " << que->Size() << endl;
-    ////testLinkedListInt();
-    //cout << endl;
-    //testDynamicArrayChar();
-    
+    int n, q;
+    cin >> n >> q;
+    LinkedListSequence<int>* peopleStatistic = new LinkedListSequence<int>;
+    Queue<int>* que = new Queue<int>;
+    Queue<int>* que_buf_1 = new Queue<int>;
+    Queue<int>* que_buf_2 = new Queue<int>;
+    for (int i = 1; i <= n; i++)
+    {
+        peopleStatistic->Append(0);
+    }
+    int type, id;
+    for (int k = 0;k<q;k++)
+    {
+        cin >> type >> id;
+        if (type == 1)
+        {
+            que->push(id);
+        }
+        else
+        {
+            for (int i = 0; i < que->Size(); i++)
+            {
+                if (que->Peek(i) == id)
+                {
+                    que_buf_1 = que->GetSubsequence(0, i-1);
+                    que_buf_2 = que->GetSubsequence(i+1, que->Size()-1);
+                    que_buf_1->Concat(*que_buf_2);
+                    delete que;
+                    que = que_buf_1;
+                    delete que_buf_2;
+                }
+            }
+        }
+        int temporary_id = 0;
+        for (int i = 0; i < que->Size(); i++)
+        {
+            temporary_id = que->Peek(i);
+            peopleStatistic->Set(temporary_id, peopleStatistic->Get(temporary_id)+1);
+        }
+    }
+    int happiest_id = 0, max_time = 0;
+    int temporary_time=0;
+    for (int i = 0; i < n; i++)
+    {
+        temporary_time = peopleStatistic->Get(i);
+        if (temporary_time > max_time)
+        {
+            happiest_id = i;
+            max_time = temporary_time;
+        }
+    }
+    cout << happiest_id<<" "<< max_time;
     system("pause>0");
 }
 
